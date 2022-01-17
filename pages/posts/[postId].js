ad31f7fd9c9c867ca846efbf367394c8b0bab480
Nicolas Bürgler <trajan15.nb@gmail.com>
2022-01-17 10:07:58 +0100

    kommentarlänge validiert

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -29,9 +29,10 @@ export default function Post({ post }) {
                     >
                         <p>Your Comment:</p>
                         <textarea
+                            maxlength="200"
                             id="commentContent"
                             name="commentContent"
-                            rows="5"
+                            rows="3"
                             className="resize-none mt-5 px-3 py-2 border shadow-sm border-gray-300 placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
                         ></textarea>
                         <input
